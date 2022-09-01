60959e46cc1993a84a5fd43730b3d0ed01a33e45
Nick Hopkins <87438259+nhopkins19@users.noreply.github.com>
2022-09-01 14:00:42 -0500

    Update slices.go

---
+++ slices/slices.go
@@ -0,0 +1,14 @@
+package slices
+
+//RemoveDuplicates removes duplicate entries from a slice of string or int
+func RemoveDuplicates[T string | int](sliceList []T) []T {
+allKeys := make(map[T]bool)
+list := []T{}
+for _, item := range sliceList {
+if _, value := allKeys[item]; !value {
+allKeys[item] = true
+list = append(list, item)
+}
+}
+return list
+}
