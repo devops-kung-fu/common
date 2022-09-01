7cc88233a5733703ddacd9f1a406d5005cdab6ef
Nick Hopkins <87438259+nhopkins19@users.noreply.github.com>
2022-09-01 14:00:14 -0500

    Update slices-test.go

---
+++ slices/slices-test.go
@@ -0,0 +1,18 @@
+package slices
+
+import (
+"testing"
+
+"github.com/stretchr/testify/assert"
+)
+
+func Test_RemoveDuplicates(t *testing.T) {
+test := []string{"A", "B", "C", "D"}
+
+result := RemoveDuplicates(test)
+assert.Len(t, result, 4)
+
+test = append(test, "B")
+result = RemoveDuplicates(test)
+assert.Len(t, result, 4)
+}
